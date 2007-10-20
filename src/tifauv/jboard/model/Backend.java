2f49c4a64299fddb0be1af23be509c6af3ebce69
tifauv <tifauv@b2994817-983e-0410-8707-b54380f182b0>
2007-10-20 00:20:21 +0000

    Initial import.

---
+++ src/tifauv/jboard/model/Backend.java
@@ -0,0 +1,60 @@
+/**
+ * 19 oct. 07
+ */
+package tifauv.jboard.model;
+
+/**
+ * 
+ *
+ * @version 0.1
+ *
+ * @author Olivier Serve <olivier.serve@bull.net>
+ */
+public class Backend {
+
+	// STATIC FIELDS \\
+	private static Backend s_instance;
+	
+	
+	// FIELDS \\
+	private History m_history;
+	
+
+	// CONSTRUCTORS \\
+	private Backend() {
+		m_history = new History("http://localhost/jboard");
+	}
+	
+
+	// GETTERS \\
+	public final String getLastModified() {
+		return m_history.getLastModified();
+	}
+	
+	public final String getText() {
+		return m_history.toString();
+	}
+	
+	public final synchronized String getText(String p_modifiedSince) {
+		if (m_history.isModifiedSince(p_modifiedSince))
+			return m_history.toString();
+		return null;
+	}
+	
+	public final synchronized void addMessage(String p_info, String p_message) {
+		m_history.addPost(new Post(p_info, p_message));
+	}
+	
+	public final synchronized void addMessage(String p_info, String p_message, String p_login) {
+		m_history.addPost(new Post(p_info, p_message, p_login));
+	}
+	
+
+	// METHODS \\
+	public static synchronized Backend getInstance() {
+		if (s_instance == null) {
+			s_instance = new Backend();
+		}
+		return s_instance;
+	}
+}
---
+++ src/tifauv/jboard/servlets/BackendServlet.java
@@ -0,0 +1,117 @@
+/**
+ * 19 oct. 07
+ */
+package tifauv.jboard.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import tifauv.jboard.model.Backend;
+
+/**
+ * This servlet sends the backend.
+ *
+ * @version 1.0
+ *
+ * @author Olivier Serve <tifauv@gmail.com>
+ */
+public class BackendServlet extends HttpServlet {
+
+	// CONSTANTS \\
+	/** The serialization UID. */
+	private static final long serialVersionUID = 8034380019578813009L;
+
+	/** The If-Modified-Since header name. */
+	private static final String IF_MODIFIED_SINCE = "If-Modified-Since";
+	
+	/** The Last-Modified header name.*/
+	private static final String LAST_MODIFIED = "Last-Modified";
+	
+	
+	// FIELDS \\
+	/** The logger. */
+	private Logger m_logger = Logger.getLogger(BackendServlet.class);
+	
+
+	// METHODS \\
+	/**
+	 * Ensures the Backend is created.
+	 */
+	@Override
+	public final void init() {
+		Backend.getInstance();
+		m_logger.info("Backend servlet ready.");
+	}
+	
+	
+	/**
+	 * Calls {@link #doWork(HttpServletRequest, HttpServletResponse)}.
+	 * 
+	 * @param p_request
+	 *            the HTTP request
+	 * @param p_response
+	 *            the HTTP response
+	 * 
+	 * @see #doWork(HttpServletRequest, HttpServletResponse)
+	 */
+	@Override
+	public final void doGet(HttpServletRequest p_request, HttpServletResponse p_response) {
+		m_logger.info("New GET backend request from [" + p_request.getRemoteAddr() + "].");
+		doWork(p_request, p_response);
+	}
+
+
+	/**
+	 * Calls {@link #doWork(HttpServletRequest, HttpServletResponse)}.
+	 * 
+	 * @param p_request
+	 *            the HTTP request
+	 * @param p_response
+	 *            the HTTP response
+	 * 
+	 * @see #doWork(HttpServletRequest, HttpServletResponse)
+	 */
+	@Override
+	public final void doPost(HttpServletRequest p_request, HttpServletResponse p_response) {
+		m_logger.info("New POST backend request from [" + p_request.getRemoteAddr() + "].");
+		doWork(p_request, p_response);
+	}
+	
+	
+	/**
+	 * Sends the backend if the If-Modified-Since request header is older than the
+	 * Last-Modified date of the history.
+	 * 
+	 * @param p_request
+	 *            the HTTP request
+	 * @param p_response
+	 *            the HTTP response
+	 */
+	private final void doWork(HttpServletRequest p_request, HttpServletResponse p_response) {
+		String text = Backend.getInstance().getText(p_request.getParameter(IF_MODIFIED_SINCE));
+
+		if (text == null) {
+			p_response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+			p_response.setHeader(LAST_MODIFIED, Backend.getInstance().getLastModified());
+			p_response.setContentLength(0);
+		}
+		else {
+			p_response.setStatus(HttpServletResponse.SC_OK);
+			p_response.setContentType("text/xml");
+			p_response.setHeader(LAST_MODIFIED, Backend.getInstance().getLastModified());
+			p_response.setContentLength(text.length());
+			try {
+				p_response.getWriter().print(text);
+			}
+			catch (IOException e) {
+				p_response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				p_response.setContentLength(0);
+			}
+		}
+	}
+}
