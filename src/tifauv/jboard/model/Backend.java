d41ba27f64d0c26224af4b6ec731400dd7f6ef52
tifauv <tifauv@b2994817-983e-0410-8707-b54380f182b0>
2007-10-21 12:24:13 +0000

    Added configuration support

--- src/tifauv/jboard/model/Backend.java
+++ src/tifauv/jboard/model/Backend.java
@@ -3,6 +3,11 @@
  */
 package tifauv.jboard.model;
 
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
+
 /**
  * 
  *
@@ -12,17 +17,31 @@ package tifauv.jboard.model;
  */
 public class Backend {
 
+	// CONSTANTS \\
+	public static final String DEFAULT_URL = "http://localhost:8080/jboard";
+	
+	private static final String CONFIG_PROPERTIES = "tifauv.jboard.config";
+	
+	private static final String URL_KEY = "jboard.url";
+	
+	private static final String SIZE_KEY = "jboard.size";
+	
+	
 	// STATIC FIELDS \\
 	private static Backend s_instance;
 	
 	
 	// FIELDS \\
+	/** The board's history. */
 	private History m_history;
+	
+	/** The logger. */
+	private Logger m_logger = Logger.getLogger(Backend.class);
 	
 
 	// CONSTRUCTORS \\
 	private Backend() {
-		m_history = new History("http://localhost:8080/jboard");
+		init();
 	}
 	
 
@@ -41,14 +60,6 @@ public class Backend {
 		return null;
 	}
 	
-	public final synchronized void addMessage(String p_info, String p_message) {
-		m_history.addPost(new Post(p_info, p_message));
-	}
-	
-	public final synchronized void addMessage(String p_info, String p_message, String p_login) {
-		m_history.addPost(new Post(p_info, p_message, p_login));
-	}
-	
 
 	// METHODS \\
 	public static synchronized Backend getInstance() {
@@ -56,5 +67,48 @@ public class Backend {
 			s_instance = new Backend();
 		}
 		return s_instance;
+	}
+	
+	protected final synchronized void init() {
+		String url = DEFAULT_URL;
+		int size = History.DEFAULT_SIZE;
+		try {
+			ResourceBundle config = ResourceBundle.getBundle(CONFIG_PROPERTIES);
+			try {
+				url = config.getString(URL_KEY);
+			}
+			catch (MissingResourceException e) {
+				m_logger.warn("The configuration key '" + e.getKey() + "' does not exist.");
+			}
+			
+			try {
+				size = Integer.parseInt(config.getString(SIZE_KEY));
+			}
+			catch (MissingResourceException e) {
+				m_logger.warn("The configuration key '" + e.getKey() + "' does not exist.");
+			}
+			catch (NumberFormatException e) {
+				m_logger.warn("The configuration key '" + SIZE_KEY + "' doesn't have an integer value.");
+			}
+		}
+		catch (MissingResourceException e) {
+			m_logger.error("The configuration file '" + CONFIG_PROPERTIES + "' is not in the CLASSPATH.");
+		}
+		
+		if (m_history == null)
+			m_history = new History(url, size);
+		else {
+			m_history.setURL(url);
+			m_history.setMaxSize(size);
+		}
+		m_logger.info("Backend at '" + url + "' with " + size + "-posts history ready.");
+	}
+	
+	public final synchronized void addMessage(String p_info, String p_message) {
+		m_history.addPost(new Post(p_info, p_message));
+	}
+	
+	public final synchronized void addMessage(String p_info, String p_message, String p_login) {
+		m_history.addPost(new Post(p_info, p_message, p_login));
 	}
 }
