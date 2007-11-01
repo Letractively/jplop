5bc50e6b4b9e0a250ad5345dab512af439a2b7eb
tifauv <tifauv@b2994817-983e-0410-8707-b54380f182b0>
2007-11-01 14:49:43 +0000

    Remove the debug alert

--- www/scripts/board.js
+++ www/scripts/board.js
@@ -121,7 +121,6 @@ function httpPost(p_url, p_request, p_callback) {
 function sendMessage(p_url) {
 	var input = document.getElementById('message');
 	var request = input.getAttribute('name') + '=' + encodeURIComponent(input.value);
-	alert(request);
 	return httpPost(p_url, request, handlePostResponse);
 }
 
