36e587316c74c5237d82166e84f4bd86c915bf71
tifauv <tifauv@b2994817-983e-0410-8707-b54380f182b0>
2008-04-20 09:49:03 +0000

    Moved CommonConstants to the root package, as it is now shared accross the app

--- trunk/src/main/java/tifauv/jplop/servlets/PostServlet.java
+++ trunk/src/main/java/tifauv/jplop/servlets/PostServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import tifauv.jplop.Backend;
+import tifauv.jplop.CommonConstants;
 import tifauv.jplop.auth.User;
 
 
