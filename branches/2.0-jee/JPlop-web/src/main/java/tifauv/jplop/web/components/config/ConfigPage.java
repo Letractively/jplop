77b1df26752fca113eeef554da62201eb90509a4
tifauv@gmail.com <tifauv@gmail.com@b2994817-983e-0410-8707-b54380f182b0>
2008-08-02 15:17:34 +0000

    [2.0-jee][JPlop-web] Set the page structure along with the data labels. Real values will come in later.

--- branches/2.0-jee/JPlop-web/src/main/java/tifauv/jplop/web/components/config/ConfigPage.java
+++ branches/2.0-jee/JPlop-web/src/main/java/tifauv/jplop/web/components/config/ConfigPage.java
@@ -5,6 +5,7 @@ package tifauv.jplop.web.components.config;
 
 import org.apache.wicket.markup.html.WebPage;
 
+import org.apache.wicket.markup.html.basic.Label;
 import tifauv.jplop.web.components.footer.FooterPanel;
 import tifauv.jplop.web.components.header.HeaderPanel;
 
@@ -23,6 +24,20 @@ public class ConfigPage extends WebPage {
 	 */
 	public ConfigPage() {
 		add(new HeaderPanel("header"));
+		
+		add(new Label("url-discover", "http://localhost:8080/jplop/discover"));
+		add(new Label("site", "JPlop"));
+		add(new Label("title", "Da JEE Board"));
+		add(new Label("baseurl", "http://localhost:8080/jplop"));
+
+		add(new Label("config-site", "JPlop"));
+		add(new Label("config-login", "Da JEE Board"));
+		add(new Label("url-backend", "http://localhost:8080/jplop/backend"));
+		add(new Label("url-post", "http://localhost:8080/jplop/post"));
+		add(new Label("config-maxlength", "512"));
+		add(new Label("config-site2", "JPlop"));
+		add(new Label("session-id", "DEADBEEF"));
+
 		add(new FooterPanel("footer"));
 	}
 }
